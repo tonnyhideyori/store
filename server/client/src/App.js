161e9b8c67a1e3d0dcaa68175cf6a734b9c9170b
edwin ismail <typhoon_ed@yahoo.com>
2018-12-13 14:00:52 +0100

    front-end debugging

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
             new web app for your inventory management coming soon......<br/>
             it uses Nodejs(back-end) and react-redux app(front-end)
