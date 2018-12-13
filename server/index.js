c505414bbd764400f58ea7d93adcaf67b2eb774f
edwin ismail <typhoon_ed@yahoo.com>
2018-12-13 14:07:26 +0100

    front-end debug

--- server/index.js
+++ server/index.js
@@ -34,12 +34,13 @@ app.use("/",sell)
 
 if(process.env.NODE_ENV==="production"){
     //express will serve up production assets like our main.js files
-    app.use(express.static('client/build'))
+    app.use(express.static("client/build"));
 
-    //express will serve up our index.html,if it doesnt recognize the route
-    const path=require('path')
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client",'build','index.html'))
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+                res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
 const port = 4500 || process.env.PORT
